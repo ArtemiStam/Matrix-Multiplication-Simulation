6383c4b392bf31834be4ef25b943870ffa3e84bc
ArtemiStam <artemistamataki@gmail.com>
2024-12-04 14:49:21 +0200

    Working

--- matrix_mult_v1.c
+++ matrix_mult_v1.c
@@ -25,18 +25,38 @@ void load_dense_matrix(int8_t dense[4][4], int rs1, int rs2) {
         for (int j=3; j >= 0; j--) {
             if (i <= 1)
             {
-                dense[i][j] = rs1_data ^ 0b00000000; 
+                dense[i][j] = rs1_data & 0xFF; 
                 rs1_data = rs1_data>>8;
             }
             else {
-                dense[i][j] = rs2_data ^ 0b00000000;
+                dense[i][j] = rs2_data & 0xFF;
                 rs2_data = rs2_data>>8;
             }
         }
     }
 }
 
+/*rs1 contains the sparse matrix,
+  rs2 contains the metadata of the sparse matrix*/
+void load_sparse_matrix(int8_t sparse[4][2], int8_t metadata[4][2], int rs1, int rs2) {
+    int64_t rs1_data, rs2_data;
+
+    rs1_data = REG_FILE[rs1];
+    rs2_data = REG_FILE[rs2];
+
+    for (int i=3; i >= 0; i--) {
+        for (int j=1; j >= 0; j--) {
+            sparse[i][j] = rs1_data & 0xFF; 
+            rs1_data = rs1_data>>8;
+          
+            metadata[i][j] = rs2_data & 0xFF;
+            rs2_data = rs2_data>>8;
+        }
+    }
+}
+
 //void reset_accumulator()
+void print_matrix(int **matrix, int rows, int cols);
 
 void accelerator_unit(int32_t instr) {
     static int8_t sparse[4][2], meta_data[4][2], dense[4][4];
@@ -48,7 +68,7 @@ void accelerator_unit(int32_t instr) {
     rs1 = (instr >> 15) & 31;   //mask is 31=2^5-1 because rs1 is 5bits long
     rs2 = (instr >> 20) & 31;   //mask is 31=2^5-1 because rs2 is 5bits long
 
-    printf("HERE: opcode: %d funct3: %d rs1: %d rs2: %d\n", opcode, funct3, rs1, rs2);
+    //printf("HERE: opcode: %d funct3: %d rs1: %d rs2: %d\n", opcode, funct3, rs1, rs2);
     if (opcode == OPCODE) {
         switch (funct3) {
             case LOAD_DENSE:
@@ -64,6 +84,41 @@ void accelerator_unit(int32_t instr) {
                 break;
 
             case LOAD_SPARSE:
+                load_sparse_matrix(sparse, meta_data, rs1, rs2);
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        printf("%d ", sparse[i][j]);
+                    }
+                    printf("\n");
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        printf("%d ", meta_data[i][j]);
+                    }
+                    printf("\n");
+                }
+
+                for (int i = 0; i < 4; i++) {  // Rows in the current block
+                    for (int j = 0; j < 4; j++) {  // Columns in the current block
+                        for (int k = 0; k < 2; k++) {  // Overlapping blocks
+                            ACC[i][j] += sparse[i][k] * dense[meta_data[i][k]][j];  // Dot product for the block
+                        }
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        printf("%d ", ACC[i][j]);
+                    }
+                    printf("\n");
+                }
                 break;
 
             case RESET_ACC:
@@ -108,10 +163,9 @@ int main(void) {
     int8_t sparse[8][4]={{1,4,5,5},{4,8,5,6},{5,7,9,4},{4,6,9,3},{6,5,8,3},{6,5,8,3},{6,5,8,3},{6,5,8,3}};  //Pressumed matrix is pre-compressed
     int8_t dense[8][8]={{1,4,5,6,2,8,8,3},{7,9,6,8,5,8,7,8},{3,4,5,6,8,7,5,3},{1,4,5,6,2,8,8,3},{7,9,6,8,5,8,7,8},{3,4,5,6,8,7,5,3},{1,4,5,6,2,8,8,3},{7,9,6,8,5,8,7,8}};
     int8_t metadata[8][4] = {{0,2,1,2},{2,3,1,2},{0,3,2,3},{0,2,2,3},{1,3,0,2},{2,3,0,1},{1,2,1,2},{2,3,1,2}};
-    int32_t final[8][8] = {{0},{0},{0},{0},{0},{0},{0},{0}};
-    int sparse_columns;
-    int B=4, M=8, K=4, N=8; //B is the block dimensions, M is the num of sparse matrix rows, K is the num of sparse columns(un-compressed) and dense columns, N is the num of dense columns.
-    int rs1, rs2, block_num = 0;
+    int32_t final[8][8] = {{0},{0},{0},{0},{0},{0},{0},{0}}, final_test[8][8] = {{33,60,75,90,84,111,93,45},{41,92,115,138,100,175,157,69},{49,120,129,158,62,200,196,95},{52,103,113,138,59,170,155,81},{106,158,124,160,86,179,162,136},{88,128,118,148,122,167,141,106},{84,118,116,144,140,163,131,96},{50,88,110,132,128,162,134,66}};
+    int B=4, M=8, K=8, N=8; //B is the block dimensions, M is the num of sparse matrix rows, K is the num of sparse columns(un-compressed) and dense columns, N is the num of dense columns.
+    int rs1, rs2;
 
 
     /*Call nullifying instr to initialize the accelerator accumulators*/
@@ -124,28 +178,30 @@ int main(void) {
               1. Software has to place the dense block into 2 registers in the register file so that the accelerator can read it.
               2. Call the accelerator instr to load the dense block into the accelerator.
             */
-            rs1 = 10;
-            rs2 = 11;
-            REG_FILE[rs1] = 0;
-            REG_FILE[rs2] = 0;
-            for (int row = ii; row < MIN(ii + B, M); row++) /*Concut all the elements of the block*/
-            {
-                for (int col = jj; col < MIN(jj + B, N); col++)
-                {
-                    if ((row-ii) < 2)
-                    {
-                        REG_FILE[rs1] = (REG_FILE[rs1] << 8) ^ dense[row][col];
-                    } else {
-                        REG_FILE[rs2] = (REG_FILE[rs2] << 8) ^ dense[row][col];
-                    }
-                    
-                }
-            }
-            printf("reg_file[%d]:%lx reg_file[%d]:%lx\n",block_num++, rs1, REG_FILE[rs1], rs2, REG_FILE[rs2]);
-            accelerator_unit(0b00000000101101010001000000001011); //funct7:0 rs2:11 rs1:10 funct3:1 rd:0 opcode:11
 
-            for (int kk = 0; kk < K; kk += B/2) {    // Block columns of Y and rows of Z
-                sparse_columns =  kk/2; //because matrix is sparse with 50% sparisty(2:1) so we use half the columns
+            for (int kk = 0; kk < K; kk += B) {    // Block columns of Y and rows of Z
+                rs1 = 10;
+                rs2 = 11;
+                REG_FILE[rs1] = 0;
+                REG_FILE[rs2] = 0;
+                for (int row = kk; row < MIN(kk + B, K); row++) /*Concut all the elements of the block*/
+                {
+                    for (int col = jj; col < MIN(jj + B, N); col++)
+                    {
+                        if ((row - kk) < 2)
+                        {
+                            REG_FILE[rs1] = (REG_FILE[rs1] << 8) ^ dense[row][col];
+                        }
+                        else
+                        {
+                            REG_FILE[rs2] = (REG_FILE[rs2] << 8) ^ dense[row][col];
+                        }
+                    }
+                }
+                //printf("reg_file[%d]:%lx reg_file[%d]:%lx\n", rs1, REG_FILE[rs1], rs2, REG_FILE[rs2]);
+                accelerator_unit(0b00000000101101010001000000001011); // funct7:0 rs2:11 rs1:10 funct3:1 rd:0 opcode:11
+
+                //because matrix is sparse with 50% sparisty(2:1) so we use half the columns
                 /*Here the sparse block is known.
                   1. The software has to extract the sparse block and its metadata to the register file.
                   2. Call accelerator instruction to load the sparse block and metadata into the accelerator.
@@ -158,32 +214,27 @@ int main(void) {
                 REG_FILE[rs2] = 0;
                 for (int sparse_row = ii; sparse_row < MIN(ii + B, M); sparse_row++) /*Concut all the elements of the block*/
                 {
-                    for (int sparse_col = kk; sparse_col < MIN(kk + B/2, K); sparse_col++)
+                    for (int sparse_col = kk/2; sparse_col < MIN(kk/2 + B/2, K/2); sparse_col++)
                     {
                         REG_FILE[rs1] = (REG_FILE[rs1] << 8) ^ sparse[sparse_row][sparse_col];
                         REG_FILE[rs2] = (REG_FILE[rs2] << 8) ^ metadata[sparse_row][sparse_col];
                     }
                 }
-                printf("reg_file[%d]:%lx reg_file[%d]:%lx\n", rs1, REG_FILE[rs1], rs2, REG_FILE[rs2]);
-
-                /* This code is implemented inside the accelerator instead of software
-                for (int i = ii; i < MIN(ii + B, M); i++) {  // Rows in the current block
-                    for (int j = jj; j < MIN(jj + B, N); j++) {  // Columns in the current block
-                        double r = 0.0;
-                        for (int k = kk; k < MIN(kk + B, K); k++) {  // Overlapping blocks
-                            r += sparse[i][k] * dense[k][j];  // Dot product for the block
-                        }
-                        final[i][j] += r; // Accumulate the result
-                        print_matrix(final,M, N);
-                    }
-                }*/
+                //printf("reg_file[%d]:%lx reg_file[%d]:%lx\n", rs1, REG_FILE[rs1], rs2, REG_FILE[rs2]);
+                accelerator_unit(0b00000000110101100010000000001011); // funct7:0 rs2:12 rs1:13 funct3:2 rd:0 opcode:11
             }
             /*When the block of the dense matrix changes to a new block column-wise we need to save the accumulators since the final matrix block is finished and will not change,
               also nullify the accumulators inside the acclerator to 
               1. For every accumulator call instr that saves two accumulators(32b + 32b = 64b) column-wise to a register
               2. Call nullifying instr for accelerator accumulators
             */
+            rs1=14;
+            rs2=15;
+
+            
+            accelerator_unit(0b0001011); //reset accumulator for new block
         }
+        //accelerator_unit(0b0001011);
         /*When the block of the sparse matrix changes row-wise we also need to nullify the changes and save the previous accumulator values
             1. For every accumulator call instr that saves two accumulators(32b + 32b = 64b) column-wise to a register
             2. Call nullifying instr for accelerator accumulators
