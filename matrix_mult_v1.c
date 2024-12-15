3c79d320bf689dfe3e9f40d9b8d27b4d5dc6a3e2
ArtemiStam <artemistamataki@gmail.com>
2024-12-15 12:24:37 +0200

    Working CVA6 simulation with professor suggestions

--- matrix_mult_v1.c
+++ matrix_mult_v1.c
@@ -176,16 +176,13 @@ void print_matrix(int **matrix, int rows, int cols) {
 }
 
 int main(void) {
-  /*  int64_t rs[32] = {0}; //initialize all regs in regfile with 0
-    int8_t sparse_block[4][2], meta_data_block[4][2], dense_block[4][4];
-    int32_t final[4][4];*/
     int8_t sparse[8][4]={{1,4,5,5},{4,8,5,6},{5,7,9,4},{4,6,9,3},{6,5,8,3},{6,5,8,3},{6,5,8,3},{6,5,8,3}};  //Pressumed matrix is pre-compressed
     int8_t dense[8][8]={{1,4,5,6,2,8,8,3},{7,9,6,8,5,8,7,8},{3,4,5,6,8,7,5,3},{1,4,5,6,2,8,8,3},{7,9,6,8,5,8,7,8},{3,4,5,6,8,7,5,3},{1,4,5,6,2,8,8,3},{7,9,6,8,5,8,7,8}};
     int8_t metadata[8][4] = {{0,2,1,2},{2,3,1,2},{0,3,2,3},{0,2,2,3},{1,3,0,2},{2,3,0,1},{1,2,1,2},{2,3,1,2}};
     int32_t final[8][8] = {{0},{0},{0},{0},{0},{0},{0},{0}}, instr, final_test[8][8] = {{33,60,75,90,84,111,93,45},{41,92,115,138,100,175,157,69},{49,120,129,158,62,200,196,95},{52,103,113,138,59,170,155,81},{106,158,124,160,86,179,162,136},{88,128,118,148,122,167,141,106},{84,118,116,144,140,163,131,96},{50,88,110,132,128,162,134,66}};
     int B=4, M=8, K=8, N=8; //B is the block dimensions, M is the num of sparse matrix rows, K is the num of sparse columns(un-compressed) and dense columns, N is the num of dense columns.
     int rs1, rs2, rd;
-
+    int64_t row1, row2, row3, row4;
     
     //Call nullifying instr to initialize the accelerator accumulators
     accelerator_unit(0b0001011);
@@ -196,10 +193,35 @@ int main(void) {
             //Here the specific block we want to process is known. 
               //1. Software has to place the dense block into 2 registers in the register file so that the accelerator can read it.
               //2. Call the accelerator instr to load the dense block into the accelerator.
-            
 
             for (int kk = 0; kk < K; kk += B) {    // Block columns of Y and rows of Z
+
                 rs1 = 10;
+                rs2 = 11;
+                row1 = (dense[kk][jj] << 24) | (dense[kk][jj+1] << 16) | (dense[kk][jj+2] << 8) | dense[kk][jj+3];
+                row2 = (dense[kk+1][jj] << 24) | (dense[kk+1][jj+1] << 16) | (dense[kk+1][jj+2] << 8) | dense[kk+1][jj+3];
+                row3 = (dense[kk+2][jj] << 24) | (dense[kk+2][jj+1] << 16) | (dense[kk+2][jj+2] << 8) | dense[kk+2][jj+3];
+                row4 = (dense[kk+3][jj] << 24) | (dense[kk+3][jj+1] << 16) | (dense[kk+3][jj+2] << 8) | dense[kk+3][jj+3];
+                
+                REG_FILE[rs1] = ((row1 << 32) ^ row2);
+                REG_FILE[rs2] = ((row3 << 32) ^ row4);
+                accelerator_unit(0b00000000101101010001000000001011); // funct7:0 rs2:11 rs1:10 funct3:1 rd:0 opcode:11
+
+                rs1 = 12;
+                rs2 = 13;
+                row1 = (sparse[ii][kk/2] << 8)   | sparse[ii][kk/2+1];
+                row2 = (sparse[ii+1][kk/2] << 8) | sparse[ii+1][kk/2+1];
+                row3 = (sparse[ii+2][kk/2] << 8) | sparse[ii+2][kk/2+1];
+                row4 = (sparse[ii+3][kk/2] << 8) | sparse[ii+3][kk/2+1];
+                REG_FILE[rs1] = (((((row1 << 16) ^ row2) << 16) ^ row3) << 16) ^ row4;
+                
+                row1 = (metadata[ii][kk/2] << 8)   | metadata[ii][kk/2+1];
+                row2 = (metadata[ii+1][kk/2] << 8) | metadata[ii+1][kk/2+1];
+                row3 = (metadata[ii+2][kk/2] << 8) | metadata[ii+2][kk/2+1];
+                row4 = (metadata[ii+3][kk/2] << 8) | metadata[ii+3][kk/2+1];
+                REG_FILE[rs2] = (((((row1 << 16) ^ row2) << 16) ^ row3) << 16) ^ row4;
+                accelerator_unit(0b00000000110101100010000000001011); // funct7:0 rs2:12 rs1:13 funct3:2 rd:0 opcode:11
+                /*rs1 = 10;
                 rs2 = 11;
                 REG_FILE[rs1] = 0;
                 REG_FILE[rs2] = 0;
@@ -240,7 +262,7 @@ int main(void) {
                     }
                 }
                 //printf("reg_file[%d]:%lx reg_file[%d]:%lx\n", rs1, REG_FILE[rs1], rs2, REG_FILE[rs2]);
-                accelerator_unit(0b00000000110101100010000000001011); // funct7:0 rs2:12 rs1:13 funct3:2 rd:0 opcode:11
+                accelerator_unit(0b00000000110101100010000000001011); // funct7:0 rs2:12 rs1:13 funct3:2 rd:0 opcode:11*/
             }
             //When the block of the dense matrix changes to a new block column-wise we need to save the accumulators since the final matrix block is finished and will not change,
               //also nullify the accumulators inside the acclerator to 
@@ -269,11 +291,11 @@ int main(void) {
     {
         for (int j = 0; j < 8; j++)
         {
-            printf("%6.2d ", final[i][j]); //the prints here prevent from cva6 simulation
+            //printf("%6.2d ", final[i][j]); //the prints here prevent from cva6 simulation
         }
-        printf("\n"); //the prints here prevent from cva6 simulation
+        //printf("\n"); //the prints here prevent from cva6 simulation
     }
-    printf("\n"); //the prints here prevent from cva6 simulation
+    //printf("\n"); //the prints here prevent from cva6 simulation
     
     return 0;
 }
